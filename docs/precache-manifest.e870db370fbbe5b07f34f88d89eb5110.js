f1b0b9ee3ae7f840f734397ece416a3201cbc3e8
Carlos Ricardo Ortiz Sordo <ricardosordo82@gmail.com>
2020-08-14 15:40:54 -0500

    Adding the Build environment to gitHubPages

---
+++ docs/precache-manifest.e870db370fbbe5b07f34f88d89eb5110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fab2fed44420f77be0362858c294084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7ee54280f6f43f8060c",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "89a8f948dbd6bed0757f",
+    "url": "/static/css/main.4273482b.chunk.css"
+  },
+  {
+    "revision": "a7ee54280f6f43f8060c",
+    "url": "/static/js/2.7d1c5f5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7d1c5f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a8f948dbd6bed0757f",
+    "url": "/static/js/main.0b91fd3a.chunk.js"
+  },
+  {
+    "revision": "dc121dc24baa6ab012f2",
+    "url": "/static/js/runtime-main.3b1f43f0.js"
+  }
+]);
